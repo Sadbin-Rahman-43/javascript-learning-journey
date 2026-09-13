b78bd3b96e17eb31b3b33b732855573befa99332
Sadbin-Rahman-43 <sadbinrahman72@gmail.com>
2026-09-14 00:54:35 +0600

    blackjack complete

--- 3. Build a Blackjack game/51. Create your first object/index.js
+++ 3. Build a Blackjack game/51. Create your first object/index.js
@@ -2,4 +2,10 @@
 // It should contain at least one boolean, one string, one number, and one array
 // Log out at least two of the keys using the dot notation
 
-
+let airbnb = {
+  name: "Redison - blue",
+  hotelRating: 4.5,
+  hasRooms: true,
+  RoomType: ["ac", "non - ac"],
+};
+console.log(airbnb.name,airbnb.hotelRating)
